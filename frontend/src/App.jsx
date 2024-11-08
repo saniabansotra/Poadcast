4e93eeb6bc6f81d8c1d5330d6873c3226d1efa0e
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-11-09 01:37:29 +0530

    complete frontend and integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css"
-
+import {BrowserRouter as Router} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HeroSection from "./components/HeroSection"
+import Services from "./components/Services"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import {Toaster} from "react-hot-toast"
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Navbar/>
+      <HeroSection/>
+      <Services/>
+      <About/>
+      <Contact/>
+      <Footer/>
+      <Toaster/>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Event</h4>
+      <h2>ABOUT US</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+        deserunt beatae eius ab vel expedita ratione aliquid saepe iusto tempore
+        nulla obcaecati nobis aliquam excepturi unde enim omnis, mollitia dicta
+        sit voluptate voluptatem est aperiam! Magnam, rem! Illum quidem quam,
+        eveniet consectetur aliquam error! Sunt alias earum culpa incidunt aut,
+        molestiae beatae ducimus nihil quas vitae ab dolore, ad commodi odit
+        voluptate numquam sed saepe! Blanditiis, iusto debitis reprehenderit
+        officiis id labore odio at quo cupiditate sunt numquam eos assumenda
+        nostrum pariatur quaerat veritatis corrupti deserunt reiciendis
+        accusamus praesentium rem in deleniti adipisci. Pariatur necessitatibus
+        deleniti, voluptatibus enim dolorum sunt!
+      </p>
+    </section>
+  );
+};
+
+export default About;
